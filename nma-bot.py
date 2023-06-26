7e7771135aefdc0e659078952bf186a12bf022d2
Kevin Rusch <SEP271905@protonmail.com>
2023-06-26 04:29:17 -0400

    removing obsolete embed

--- nma-bot.py
+++ nma-bot.py
@@ -22,38 +22,6 @@ with open('servers.json') as f:
     master_db = json.load(f)
 
 
-masterEmb = discord.Embed(
-    title="",
-    url="https://www.neuromatch.io",
-    description="Click on the appropriate reactions below to initiate a bot action. If that doesn't work, you can use the commands listed below as a back-up.",
-    color=0xA400D1,
-)
-masterEmb.set_author(
-    name="Administrative Interface",
-    url="https://www.neuromatch.io",
-    icon_url="https://i.imgur.com/NwOh9XV.png",
-)
-masterEmb.add_field(
-    name="Reaction-Based Interface",
-    value="To repod a user, click :busts_in_silhouette:.\nTo merge two pods, click :people_hugging:\nTo check a user, click 🕵️.\nTo assign a user to multiple pods, click :zap:.\nTo unassign a user from multiple pods, click :cloud_tornado:.",
-    inline=False,
-)
-masterEmb.add_field(
-    name="Pod-Related Commands",
-    value="To repod a user, use `--nma repod <discord-id> <user-email> <new-pod>`.   \nTo merge two pods, use `--nma podmerge <old-pod> <new-pod>`. \nTo assign a user to pods, use `--nma assign <user-id> <pod-n>`.                 \nTo unassign a user from pods, use `--nma unassign <user-id> <pod-n>`.     \nTo check who has verified for a pod, use `--nma podcheck` in the pod channel.",
-    inline=False,
-)
-masterEmb.add_field(
-    name="Hotfix Commands",
-    value="To give interactive students access to all public channels, use `--nma unlock`. \nTo grant lead TAs access to all their pods, use `--nma leadfix`. \nTo restore TA access to their pods, use `--nma tafix`.",
-    inline=False,
-)
-masterEmb.add_field(
-    name="Debugging Commands",
-    value="To check whether you can use administrative commands, use `--nma auth`.\nTo print all variables to console, use `--nma debug`.",
-    inline=False,
-)
-
 # Actual Discord bot.
 class nmaClient(discord.Client):
     async def on_ready(self):
