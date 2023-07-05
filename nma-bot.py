3e2a1a1f00324d6b346693b21ce4df52b0843700
Konstantine Tsafatinos <kosta.tsaf@gmail.com>
2023-07-04 23:13:21 -0400

    change vc del chan logs to embed 2

--- nma-bot.py
+++ nma-bot.py
@@ -116,7 +116,7 @@ client = nmaClient(intents=intents)
 
 async def delete_channel_after(vc):
     bot_chan = discord.utils.get(vc.guild.channels, name='bot-log')
-    await bot_chan.send(embed=interact.send_embed('custom', 'Social' f'Voice Channel {vc.name} is empty, deleting after 5 minutes...'))
+    await bot_chan.send(embed=interact.send_embed('custom', 'Social', f'Voice Channel {vc.name} is empty, deleting after 5 minutes...'))
     print(f'Voice Channel {vc.name} is empty, deleting after 5 minutes...')
     await asyncio.sleep(300)
     if len(vc.members) == 0:
